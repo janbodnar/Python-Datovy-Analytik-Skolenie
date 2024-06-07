9d2841273d8f97f48da5158153152e1640b111b5
Jan Bodnar <jan.bodnar@gmail.com>
2024-06-07 16:01:13 +0200

    Update users_postgres.sql

--- data/users_postgres.sql
+++ data/users_postgres.sql
@@ -1,13 +1,15 @@
+
+DROP TABLE IF EXISTS users;
 CREATE TABLE users(id serial PRIMARY KEY, first_name VARCHAR(255), 
-				   last_name VARCHAR(255), occupation VARCHAR(255));
+		   last_name VARCHAR(255), occupation VARCHAR(255), dob VARCHAR(255));
 				   
-INSERT INTO users(first_name, last_name, occupation) VALUES('John', 'Doe', 'gardener');
-INSERT INTO users(first_name, last_name, occupation) VALUES('Roger', 'Roe', 'driver');
-INSERT INTO users(first_name, last_name, occupation) VALUES('Paul', 'Smith', 'teacher');
-INSERT INTO users(first_name, last_name, occupation) VALUES('Tomas', 'Dorak', 'programmer');
-INSERT INTO users(first_name, last_name, occupation) VALUES('Jane', 'Smith', 'teacher');
-INSERT INTO users(first_name, last_name, occupation) VALUES('Robert', 'Williams', 'driver');
-INSERT INTO users(first_name, last_name, occupation) VALUES('Erik', 'Bednar', 'programmer');
-INSERT INTO users(first_name, last_name, occupation) VALUES('Stanislav', 'Molnar', 'gardener');
-INSERT INTO users(first_name, last_name, occupation) VALUES('Kristian', 'Kalinak', 'programmer');
-INSERT INTO users(first_name, last_name, occupation) VALUES('Karol', 'Truban', 'architect');
+INSERT INTO users(first_name, last_name, occupation, dob) VALUES('John', 'Doe', 'gardener', '1978-02-11');
+INSERT INTO users(first_name, last_name, occupation, dob) VALUES('Roger', 'Roe', 'driver', '1977-12-01');
+INSERT INTO users(first_name, last_name, occupation, dob) VALUES('Paul', 'Smith', 'teacher', '1988-05-07');
+INSERT INTO users(first_name, last_name, occupation, dob) VALUES('Tomas', 'Dorak', 'programmer', '2002-06-19');
+INSERT INTO users(first_name, last_name, occupation, dob) VALUES('Jane', 'Smith', 'teacher', '1968-10-10');
+INSERT INTO users(first_name, last_name, occupation, dob) VALUES('Robert', 'Williams', 'driver', '2001-07-17');
+INSERT INTO users(first_name, last_name, occupation, dob) VALUES('Erik', 'Bednar', 'programmer', '2000-05-29');
+INSERT INTO users(first_name, last_name, occupation, dob) VALUES('Stanislav', 'Molnar', 'gardener', '1998-12-01');
+INSERT INTO users(first_name, last_name, occupation, dob) VALUES('Kristian', 'Kalinak', 'programmer', '1994-03-22');
+INSERT INTO users(first_name, last_name, occupation, dob) VALUES('Karol', 'Truban', 'architect', '1987-09-15');
