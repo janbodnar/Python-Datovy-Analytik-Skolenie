13346d7f1d11127127d88aff73dd8246525c95bb
Jan Bodnar <jan.bodnar@gmail.com>
2024-06-07 15:15:22 +0200

    Create users.sql

---
+++ data/users.sql
@@ -0,0 +1,13 @@
+CREATE TABLE users(id serial PRIMARY KEY, first_name VARCHAR(255), 
+				   last_name VARCHAR(255), occupation VARCHAR(255));
+				   
+INSERT INTO users(first_name, last_name, occupation) VALUES('John', 'Doe', 'gardener');
+INSERT INTO users(first_name, last_name, occupation) VALUES('Roger', 'Roe', 'driver');
+INSERT INTO users(first_name, last_name, occupation) VALUES('Paul', 'Smith', 'teacher');
+INSERT INTO users(first_name, last_name, occupation) VALUES('Tomas', 'Dorak', 'programmer');
+INSERT INTO users(first_name, last_name, occupation) VALUES('Jane', 'Smith', 'teacher');
+INSERT INTO users(first_name, last_name, occupation) VALUES('Robert', 'Williams', 'driver');
+INSERT INTO users(first_name, last_name, occupation) VALUES('Erik', 'Bednar', 'programmer');
+INSERT INTO users(first_name, last_name, occupation) VALUES('Stanislav', 'Molnar', 'gardener');
+INSERT INTO users(first_name, last_name, occupation) VALUES('Kristian', 'Kalinak', 'programmer');
+INSERT INTO users(first_name, last_name, occupation) VALUES('Karol', 'Truban', 'architect');
